e688068ea70a35dbda06667a454d563b3509de8c
vincerg1 <vincerg1@gmail.com>
2026-02-19 13:20:38 +0100

    UPDATE 19FEB26/1316HRS

--- back/routes/venta.js
+++ back/routes/venta.js
@@ -357,7 +357,6 @@ router.post('/direct-pay', async (req, res) => {
     });
   }
 });
-console.log("✅ payload.items[0].ingredients:", payload.items?.[0]?.ingredients);
 router.post('/pedido', async (req, res) => {
   const appMeta = await prisma.appMeta.findUnique({ where: { id: 1 } }).catch(() => null);
 
